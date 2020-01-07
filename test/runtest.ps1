7996aba44ca09fb88ea138dbd212d377eca20466
ec_fujiwara <takumi_fujiwara@earlycross.co.uk>
2020-01-07 10:47:46 +0900

    small fix on test script

--- test/runtest.ps1
+++ test/runtest.ps1
@@ -2,11 +2,11 @@
 $container_name = docker run -d -p "8100:8000" amazon/dynamodb-local -jar DynamoDBLocal.jar -sharedDb
 
 # generate table API
-pushd ..
+Push-Location ..
 go build -o gen.exe
 .\gen.exe -def .\test\tbldef.test.yaml -out .\test\ddbtbl
-rm .\gen.exe
-popd
+Remove-Item .\gen.exe
+Pop-Location
 
 # run tests
 go test .
